2b43310fb10f7cf9a1e9c63a5ef08772bf919aa6
phamnutuyetan <14520010@gm.uit.edu.vn>
2017-11-22 18:26:33 +0700

    Them load bang diem danh theo ngay

---
+++ QLNT/BusinessLayer/DiemDanhBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNT.Entities;
+using QLNT.DataLayer;
+
+namespace QLNT.BusinessLayer
+{
+    class DiemDanhBLL
+    {
+        public static object LaySoDiemDanhLop(Lop lop, string ngaydiemdanh)
+        {
+            return DiemDanhDAL.LaySoDiemDanhLop(lop, ngaydiemdanh);
+        }
+    }
+}
---
+++ QLNT/DataLayer/DiemDanhDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNT.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QLNT.DataLayer
+{
+    class DiemDanhDAL
+    {
+        public static DataTable LaySoDiemDanhLop(Lop lop, string ngaydiemdanh)
+        {
+            DataAccessHelper db = new DataAccessHelper();
+            SqlCommand cmd = null;
+            
+                cmd = db.Command("LAY_SO_DIEM_DANH_LOP");
+                cmd.Parameters.AddWithValue("@MaLop", lop.MaLop);
+                cmd.Parameters.AddWithValue("@NgayDiemDanh", ngaydiemdanh);
+
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            db.dt = new DataTable();
+            da.Fill(db.dt);
+
+            return db.dt;
+        }
+    }
+}
