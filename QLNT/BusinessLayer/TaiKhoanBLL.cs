0d53112ca4de2cbb6471e145238cecc758bd9b3f
vthhanh <vthhanh96@gmail.com>
2017-11-24 17:01:58 +0700

    them ham get ten nguoi dung

---
+++ QLNT/BusinessLayer/TaiKhoanBLL.cs
@@ -0,0 +1,23 @@
+﻿using QLNT.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNT.BusinessLayer
+{
+    class TaiKhoanBLL
+    {
+        public static string GetTenNguoiDung(string tenDangNhap)
+        {
+            foreach(DataRow row in TaiKhoanDAL.GetTenNguoiDung(tenDangNhap).Rows)
+            {
+                return row["TenHienThi"].ToString();
+            }
+
+            return "";
+        }
+    }
+}
---
+++ QLNT/DataLayer/TaiKhoanDAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNT.DataLayer
+{
+    class TaiKhoanDAL
+    {
+        public static DataTable GetTenNguoiDung(string tenDangNhap)
+        {
+            DataAccessHelper db = new DataAccessHelper();
+            SqlCommand cmd = db.Command("GetTenNguoiDung");
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@TenDangNhap", tenDangNhap);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            db.dt = new DataTable();
+            da.Fill(db.dt);
+
+            return db.dt;
+        }
+    }
+}
